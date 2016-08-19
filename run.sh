4c02a2e12fbc39364aa9dd5b6de4f8d9f4fcfbab
baranga <henning.panke@gmail.com>
2016-08-19 11:41:30 +0200

    replace env based authetication with parameters

--- run.sh
+++ run.sh
@@ -24,22 +24,11 @@ set -a
 cat /etc/vernemq/vernemq.conf.tpl | envsubst '$AUTH_ANONYMOUS $LOG_LEVEL' > /etc/vernemq/vernemq.conf
 cat /etc/default/vernemq.tpl | envsubst > /etc/default/vernemq
 
-case "$ENVIRONMENT" in
-  development)
-    # user: gfcc-broker-adapter
-    # pass: gfcc-broker-adapter
-    echo 'gfcc-broker-adapter:$6$VYzKUkOh8pFptqr8$HP6meeGp/Rk0Lcnz2cNT1cJk5uHFK7eff7TRTyyUSy5Tj7MdPHPPU2HUaOFXN6JsZhys34A+vUDDqWTuidc4Dg==' > /etc/vernemq/vmq.passwd
-    ;;
-  production)
-    # user: gfcc-broker-adapter
-    # pass: that's a secret ;)
-    echo 'gfcc-broker-adapter:$6$L37a05MtNnFwm/Hn$lkyXBknk9ix8+RoXCki5B+kGFOLvEWlXAHj/Fy1H2iOWh7hMrKIvRlwSa5c+EPqp/6UpMUXgIWR/UDQvMx6vaA==' > /etc/vernemq/vmq.passwd
-    ;;
-  *)
-    echo "invalid ENVIRONMENT: $ENVIRONMENT" 1>&2
-    exit 1
-    ;;
-esac
+# setup adapter authentication
+vmq-passwd /etc/vernemq/vmq.passwd "${GFCC_ADAPTER_USER}" <<EOS
+${GFCC_ADAPTER_PASS}
+${GFCC_ADAPTER_PASS}
+EOS
 
 set +a
 
