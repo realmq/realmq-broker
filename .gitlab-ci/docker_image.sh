398a21508079388028b0101878708233ace3da1d
baranga <henning.panke@gmail.com>
2016-09-02 10:45:47 +0200

    use dedicated script for gitlab-ci docker image build step

---
+++ .gitlab-ci/docker_image.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+if [ -z "$GITLAB_CI" -o "$GITLAB_CI" != "true" ]; then
+  echo "invalid environment" 1>&2
+  exit 1
+fi
+
+if [ -z "$CI_REGISTRY_IMAGE" ]; then
+  echo "missing CI_REGISTRY_IMAGE" 1>&2
+  exit 2
+fi
+
+if [ -z "$CI_BUILD_REF_NAME" ]; then
+  echo "missing CI_BUILD_REF_NAME" 1>&2
+  exit 3
+fi
+
+root=$(dirname $0)/..
+image_ref=$CI_REGISTRY_IMAGE:$(sed -r 's/[^a-zA-Z0-9_-]/-/g' <<< "$CI_BUILD_REF_NAME")
+
+set -ex
+docker build -t $image_ref "$root"
+docker push $image_ref
