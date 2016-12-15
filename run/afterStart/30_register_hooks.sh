d57975f7b028fe4ec6fb9b498759f64b119d22e1
baranga <baranga@posteo.de>
2016-12-15 15:33:22 +0100

    adjust hook registration

--- run/afterStart/30_register_hooks.sh
+++ run/afterStart/30_register_hooks.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 : ${ADAPTER_HOST:=broker-adapter}
-: ${ADAPTER_PORT:=80}
+: ${ADAPTER_PORT:=8080}
 
 baseUri="http://${ADAPTER_HOST}:${ADAPTER_PORT}"
-vmq-admin webhooks register hook=auth_on_register endpoint="${baseUri}/vernemq/auth_on_register"
-vmq-admin webhooks register hook=auth_on_subscribe endpoint="${baseUri}/vernemq/auth_on_subscribe"
-vmq-admin webhooks register hook=auth_on_publish endpoint="${baseUri}/vernemq/auth_on_publish"
-vmq-admin webhooks register hook=on_deliver endpoint="${baseUri}/vernemq/on_deliver"
-vmq-admin webhooks register hook=on_client_wakeup endpoint="${baseUri}/vernemq/on_client_wakeup"
-vmq-admin webhooks register hook=on_client_offline endpoint="${baseUri}/vernemq/on_client_offline"
-vmq-admin webhooks register hook=on_client_gone endpoint="${baseUri}/vernemq/on_client_gone"
+function register_hook {
+  hook=$1
+  echo "register web hook: ${hook}"
+  vmq-admin webhooks register hook=${hook} endpoint="${baseUri}/vmq/hook"
+}
+
+register_hook auth_on_register
+register_hook auth_on_subscribe
+register_hook auth_on_publish
+register_hook on_deliver
+register_hook on_client_wakeup
+register_hook on_client_offline
+register_hook on_client_gone
