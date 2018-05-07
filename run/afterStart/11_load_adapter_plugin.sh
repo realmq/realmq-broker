722828f64256e5fea10198f8ab1b6440870da7fd
baranga <baranga@posteo.de>
2018-05-07 20:57:25 +0200

    Replace lua version of topic rewrite with erlang version (#5)

---
+++ run/afterStart/11_load_adapter_plugin.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+vmq-admin plugin enable --name realmq_adapter --path /opt/broker/plugins
--- run/afterStart/11_load_topic_rewrite_on_deliver_script.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-vmq-admin script load path="/opt/broker/scripts/rewrite_topic_on_deliver.lua"
