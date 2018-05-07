c83e520fe477a2e5a3696f612f97a1479fac87a2
baranga <baranga@posteo.de>
2018-05-07 22:11:36 +0200

    Improve readability (#5)

--- plugins/adapter/src/realmq_adapter_topic.erl
+++ plugins/adapter/src/realmq_adapter_topic.erl
@@ -4,13 +4,14 @@
   to_external/1
 ]).
 
-to_external([<<"$RMQ">>|Rest]) ->
+to_external([<<"$RMQ">> | Rest]) ->
   WithoutRealm = strip_realm(Rest),
-  adjust_sync_to_external(WithoutRealm);
+  WithAdjustedSync = adjust_sync_to_external(WithoutRealm),
+  [<<"$RMQ">> | WithAdjustedSync];
 to_external(Topic) -> strip_realm(Topic).
 
-strip_realm([<<"realm">>|[_RealmId|Rest]]) -> Rest;
+strip_realm([<<"realm">>, _RealmId | Rest]) -> Rest;
 strip_realm(Topic) -> Topic.
 
-adjust_sync_to_external([<<"sync">>|[<<"user">>|[_UserId|Rest]]]) -> [<<"sync">>|[<<"my">>|Rest]];
+adjust_sync_to_external([<<"sync">>, <<"user">>, _UserId | Rest]) -> [<<"sync">>, <<"my">> | Rest];
 adjust_sync_to_external(Topic) -> Topic.
