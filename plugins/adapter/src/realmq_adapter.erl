bd8a85a6c9d092a54dc71e56620074d67659729f
Alrik Zachert <az@kreatiefer.de>
2018-06-26 15:13:06 +0200

    Add documentation.

--- plugins/adapter/src/realmq_adapter.erl
+++ plugins/adapter/src/realmq_adapter.erl
@@ -4,6 +4,8 @@
 
 -export([on_deliver/4]).
 
+%% System users (username "adapter") will see internal topic structure instead of external.
+%% That's important for retrieving the realm context.
 on_deliver(<<"adapter">>, _SubscriberId, _Topic, _Payload) -> {ok};
 on_deliver(_UserName, _SubscriberId, Topic, _Payload) ->
   error_logger:info_msg("realmq: on_deliver: Internal topic: ~p", [Topic]),
